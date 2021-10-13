1ba676c3f52500d92800e7cdb8d562afdb178dcc
Ali Hilal <ali94_200970@yahoo.com>
2021-10-13 15:41:48 +0300

    [fix] Check whether the json data is nil before trying to serialize it.

--- Sources/Scenes/CreateRequest/CreateRequestViewController.swift
+++ Sources/Scenes/CreateRequest/CreateRequestViewController.swift
@@ -444,7 +444,7 @@ private extension CreateRequestViewController {
         let countryCodeData = props.country.data(using: String.Encoding.utf8) ?? Data()
 		ticket.append(Moya.MultipartFormData(provider: .data(countryCodeData), name: "country_code"))
 
-		if let jsonData = try? JSONSerialization.data(withJSONObject: props.jsonInfo) {
+        if let jsonData = try? JSONSerialization.data(withJSONObject: props.jsonInfo ?? [:]) {
 			ticket.append(Moya.MultipartFormData(provider: .data(jsonData), name: "settings"))
 		}
 
