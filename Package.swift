af92e93a8b648c15798d99677cbe719c8e74610d
alper tabak <alpertabak@teknasyon.com>
2022-06-08 09:51:12 +0300

    [feat] Manifest file created.

---
+++ Package.swift
@@ -0,0 +1,41 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Desk360",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Desk360",
+            targets: ["Desk360"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
+        .package(url: "https://github.com/Teknasyon-Teknoloji/PersistenceKit.git", from: "0.1"),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git")
+    ],
+
+    /*
+     s.dependency "SnapKit"
+    s.dependency "Moya", '~> 14.0'
+    s.dependency "PersistenceKit"
+    s.dependency "DeviceKit"
+    s.dependency "NVActivityIndicatorView", "4.8.0"
+    */
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Desk360",
+            dependencies: []),
+        .testTarget(
+            name: "Desk360Tests",
+            dependencies: ["Desk360"]),
+    ]
+)
