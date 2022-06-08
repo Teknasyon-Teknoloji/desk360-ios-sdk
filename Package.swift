4bd52fe722cfc83cddafc4c2cdbbdb2e313f6e2f
alper tabak <alpertabak@teknasyon.com>
2022-06-08 11:10:48 +0300

    [feat] Manifest file updated.

--- Package.swift
+++ Package.swift
@@ -1,10 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Desk360",
+    platforms: [.iOS(.v10)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -16,26 +17,29 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
-        .package(url: "https://github.com/Teknasyon-Teknoloji/PersistenceKit.git", from: "0.1"),
-        .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
-        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git")
-    ],
+        .package(url: "https://github.com/Teknasyon-Teknoloji/PersistenceKit.git", .branch("master")),
+        .package(url: "https://github.com/devicekit/DeviceKit.git", .upToNextMajor(from: "4.0.0")),
+        .package(url: "https://github.com/ninjaprox/NVActivityIndicatorView.git", from: "5.1.1")
 
-    /*
-     s.dependency "SnapKit"
-    s.dependency "Moya", '~> 14.0'
-    s.dependency "PersistenceKit"
-    s.dependency "DeviceKit"
-    s.dependency "NVActivityIndicatorView", "4.8.0"
-    */
+        
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Desk360",
-            dependencies: []),
-        .testTarget(
-            name: "Desk360Tests",
-            dependencies: ["Desk360"]),
+            dependencies: [
+                "SnapKit",
+                "Moya",
+                "PersistenceKit",
+                "DeviceKit",
+                "NVActivityIndicatorView"
+
+            ],
+            path: "Sources"
+            /*resources: [
+                "Sources/Desk360.plist"
+            ]*/
+        )
     ]
 )
