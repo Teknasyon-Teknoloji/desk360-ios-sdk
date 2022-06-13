e03c84dbd321a2ebfa3d91003d9c83f363bc583c
alper tabak <alpertabak@teknasyon.com>
2022-06-13 08:56:35 +0300

    [chore] Package updates.

--- Sources/Supporting/Protocol/Loadingable.swift
+++ Sources/Supporting/Protocol/Loadingable.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import NVActivityIndicatorView
-import NVActivityIndicatorViewExtended
+//import NVActivityIndicatorViewExtended
 import SnapKit
 
 /// `Loadingable` protocol offers a convenient way to show loading in `UIView` objects.
